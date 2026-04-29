a0cec558425325106cc04f6a57ba681c0a715b2a
seo-onion <sebastianhernandezminano@gmail.com>
2026-04-29 13:22:00 -0500

    fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
+  // Solo usamos basePath, assetPrefix suele ser redundante y causar errores en GitHub Pages
   basePath: isProd ? '/prototipo-promotic' : '',
-  assetPrefix: isProd ? '/prototipo-promotic/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
